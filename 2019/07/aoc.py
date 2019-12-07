107964d0a335cbb74b0b79548ef95efb9a4a1053
Lucas Clay <38572212+mlclay@users.noreply.github.com>
2019-12-07 15:46:47 -0500

    2019-07 part 2 complete

--- 2019/07/aoc.py
+++ 2019/07/aoc.py
@@ -116,8 +116,8 @@ class Program:
                      execution_pointer=None,
                      program_memory=None,
                      instruction_parameters=None,
-                     input_queue=Queue(),
-                     output_queue=Queue()):
+                     input_queue=None,
+                     output_queue=None):
             self.extended_opcode = extended_opcode
             self.execution_pointer = execution_pointer
             self.program_memory = program_memory
@@ -133,13 +133,25 @@ class Program:
         self._memory = {}
         self._instructions = []
         self._next_instruction = 0
-        self.input_queue = Queue()
-        self.output_queue = Queue()
+        self._initialize_instruction_set()
+        self._input_queue = Queue()
+        self._output_queue = Queue()
+
+    def _initialize_instruction_set(self):
+        self._add_instruction(Instruction(1, 3, AddMethod()))
+        self._add_instruction(Instruction(2, 3, MulMethod()))
+        self._add_instruction(Instruction(3, 1, StoreInputMethod()))
+        self._add_instruction(Instruction(4, 1, OutputAddressMethod()))
+        self._add_instruction(Instruction(5, 2, JmpTrueMethod()))
+        self._add_instruction(Instruction(6, 2, JmpFalseMethod()))
+        self._add_instruction(Instruction(7, 3, LessThanMethod()))
+        self._add_instruction(Instruction(8, 3, EqualsMethod()))
+        self._add_instruction(Instruction(99, 0, HaltMethod()))
 
     def __str__(self):
         return ','.join([str(x) for x in self._memory.values()])
 
-    def add_instruction(self, instruction):
+    def _add_instruction(self, instruction):
         self._instructions.append(instruction)
 
     def get_instruction_by_opcode(self, opcode):
@@ -170,6 +182,17 @@ class Program:
     def get_memory_address(self, address):
         return self._memory[address]
 
+    def queue_input(self, value):
+        self._input_queue.put(value)
+
+    def get_output(self):
+        return self._output_queue.get()
+
+    def link_output_to(self, other):
+        if not isinstance(other, Program):
+            raise Exception(f'link_output_to requires another Program instance')
+        other._input_queue = self._output_queue
+
     def execute_next(self):
         extended_opcode = self.get_memory_address(self._next_instruction)
         instruction = self.get_instruction_by_opcode(extended_opcode)
@@ -178,15 +201,15 @@ class Program:
             extended_opcode=extended_opcode,
             execution_pointer=self._next_instruction,
             program_memory=self._memory,
-            input_queue=self.input_queue,
-            output_queue=self.output_queue,
+            input_queue=self._input_queue,
+            output_queue=self._output_queue,
         )
 
         instruction.execute(execution_context)
 
         self._next_instruction = execution_context.execution_pointer
 
-    def run(self):
+    def run_to_end(self):
         try:
             while True:
                 self.execute_next()
@@ -213,10 +236,7 @@ class MulMethod(Instruction.Method):
 class StoreInputMethod(Instruction.Method):
     def __call__(self, context):
         parameter_values = self.get_parameter_values(context)
-        try:
-            context.program_memory[parameter_values[0]] = context.input_queue.get(block=False)
-        except Empty:
-            context.program_memory[parameter_values[0]] = int(input('TEST execution requires input: '))
+        context.program_memory[parameter_values[0]] = context.input_queue.get(block=False)
         self.update_execution_pointer(context)
 
 
@@ -273,41 +293,82 @@ class HaltMethod(Instruction.Method):
         raise ProgramHalted
 
 
-def main():
-    program = Program()
-
-    program.add_instruction(Instruction(1, 3, AddMethod()))
-    program.add_instruction(Instruction(2, 3, MulMethod()))
-    program.add_instruction(Instruction(3, 1, StoreInputMethod()))
-    program.add_instruction(Instruction(4, 1, OutputAddressMethod()))
-    program.add_instruction(Instruction(5, 2, JmpTrueMethod()))
-    program.add_instruction(Instruction(6, 2, JmpFalseMethod()))
-    program.add_instruction(Instruction(7, 3, LessThanMethod()))
-    program.add_instruction(Instruction(8, 3, EqualsMethod()))
-    program.add_instruction(Instruction(99, 0, HaltMethod()))
-
-    program.initialize_memory_from_file('input.txt')
-    memory_dump = program.dump_memory()
-
+def part_one(amplifiers, memory_dump):
     phase_to_signal = []
     for phase_setting in itertools.permutations(range(5), 5):
-        for amplifier in range(5):
-            program.load_memory(memory_dump)
 
-            program.input_queue.put(phase_setting[amplifier])
-            if amplifier == 0:
-                program.input_queue.put(0)
-            else:
-                program.input_queue.put(program.output_queue.get())
+        for index, amp_name in enumerate(amplifiers.keys()):
+            amplifier = amplifiers[amp_name]
+            amplifier.load_memory(memory_dump)
+            amplifier.queue_input(phase_setting[index])
+            if index == 0:
+                amplifier.queue_input(0)
 
-            program.run()
+        for amp_name in amplifiers:
+            amplifier = amplifiers[amp_name]
+            amplifier.run_to_end()
 
         phase_to_signal.append({
             'phase': phase_setting,
-            'signal': program.output_queue.get()
+            'signal': amplifiers['E'].get_output()
         })
 
-    print(max(phase_to_signal, key=lambda x: x['signal']))
+    print(f'Part One: {max(phase_to_signal, key=lambda x: x["signal"])}')
+
+
+def part_two(amplifiers, memory_dump):
+    phase_to_signal = []
+
+    for phase_setting in itertools.permutations(range(5, 10), 5):
+
+        for index, amp_name in enumerate(amplifiers.keys()):
+            amplifier = amplifiers[amp_name]
+            amplifier.load_memory(memory_dump)
+            amplifier.queue_input(phase_setting[index])
+            if index == 0:
+                amplifier.queue_input(0)
+
+        try:
+            while True:
+                for amp_name in amplifiers:
+                    amplifier = amplifiers[amp_name]
+                    try:
+                        amplifier.execute_next()
+                    except Empty:
+                        pass
+                    except ProgramHalted as ph:
+                        if amp_name == 'E':
+                            phase_to_signal.append({
+                                'phase': phase_setting,
+                                'signal': amplifier.get_output()
+                            })
+                            raise ph
+        except ProgramHalted:
+            pass
+
+    print(f'Part Two: {max(phase_to_signal, key=lambda x: x["signal"])}')
+
+
+def main():
+    program = Program()
+    program.initialize_memory_from_file('input.txt')
+    memory_dump = program.dump_memory()
+
+    amplifiers = dict()
+    amplifier_names = ['A', 'B', 'C', 'D', 'E']
+
+    for amp_name in amplifier_names:
+        amplifiers[amp_name] = Program()
+
+    amplifiers['A'].link_output_to(amplifiers['B'])
+    amplifiers['B'].link_output_to(amplifiers['C'])
+    amplifiers['C'].link_output_to(amplifiers['D'])
+    amplifiers['D'].link_output_to(amplifiers['E'])
+    amplifiers['E'].link_output_to(amplifiers['A'])
+
+    part_one(amplifiers, memory_dump)
+
+    part_two(amplifiers, memory_dump)
 
 
 if __name__ == '__main__':
