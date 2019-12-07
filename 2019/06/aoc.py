bb30226f07340586ed8315eb4c4bd9930f54ffb1
Lucas Clay <38572212+mlclay@users.noreply.github.com>
2019-12-06 19:20:56 -0500

    2019-06 part 1 complete

---
+++ 2019/06/aoc.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+
+class SpaceObject:
+    def __init__(self, name):
+        self.name = name
+        self.orbits = None
+        self.orbited_by = []
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        if isinstance(other, SpaceObject):
+            return self.name == other.name
+        raise NotImplementedError
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f'{self.name} orbits {self.orbits.name if self.orbits else None};'
+
+    def get_depth_to(self, other):
+        return 0 if other == self else self.orbits.get_depth_to(other) + 1
+
+
+def parse_input():
+    """
+    Parse input.txt to Wire objects
+    """
+
+    space_objects = []
+
+    with open('input.txt', 'r') as txt:
+        for local_orbit in txt:
+            center, orbiter = local_orbit.strip().split(')')
+
+            try:
+                orbiter = space_objects[space_objects.index(orbiter)]
+                if orbiter.orbits:
+                    raise Exception(f'{orbiter} already in orbit!')
+            except ValueError:
+                orbiter = SpaceObject(orbiter)
+                space_objects.append(orbiter)
+
+            try:
+                center = space_objects[space_objects.index(center)]
+            except ValueError:
+                center = SpaceObject(center)
+                space_objects.append(center)
+
+            center.orbited_by.append(orbiter)
+            orbiter.orbits = center
+
+    return space_objects
+
+
+def main():
+    space_objects = parse_input()
+
+    indirect_orbits = 0
+    for space_object in space_objects:
+        indirect_orbits += space_object.get_depth_to('COM')
+
+    print(f'Part One: {indirect_orbits}')
+
+
+if __name__ == '__main__':
+    main()
