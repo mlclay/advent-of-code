71ff9ba000d1256e20c522ca4f01da403c19ea3f
Lucas Clay <38572212+mlclay@users.noreply.github.com>
2019-12-11 19:01:43 -0500

    2019-11 part 1 complete

--- 2019/11/aoc.py
+++ 2019/11/aoc.py
@@ -1,12 +1,88 @@
 #!/usr/bin/python3
+from collections import defaultdict
+from queue import Queue
+
 from intcode import IntcodeProgram
+from exceptions import WaitingForInput, ProgramHalted
+
+
+BLACK = 0
+WHITE = 1
+
+
+class Point:
+    def __init__(self, x, y, heading):
+        self.x = x
+        self.y = y
+        self.heading = heading
+
+
+class Robot:
+
+    TURN_LEFT = 0
+    TURN_RIGHT = 1
+
+    HEADING_UP = 0
+    HEADING_RIGHT = 1
+    HEADING_DOWN = 2
+    HEADING_LEFT = 3
+
+    def __init__(self):
+        self.position = Point(0, 0, 0)
+
+    def get_grid_pos(self):
+        return self.position.x, self.position.y
+
+    def move(self, direction):
+        if direction == Robot.TURN_LEFT:
+            self.position.heading = (self.position.heading + 3) % 4
+        elif direction == Robot.TURN_RIGHT:
+            self.position.heading = (self.position.heading + 1) % 4
+        else:
+            raise Exception(f'Invalid Direction {direction}')
+
+        if self.position.heading == Robot.HEADING_UP:
+            self.position.y += 1
+        elif self.position.heading == Robot.HEADING_DOWN:
+            self.position.y -= 1
+        elif self.position.heading == Robot.HEADING_RIGHT:
+            self.position.x += 1
+        elif self.position.heading == Robot.HEADING_LEFT:
+            self.position.x -= 1
+        else:
+            raise Exception(f'Invalid Heading ... somehow {self.position}')
 
 
 def main():
-    program = IntcodeProgram(io_scheme=IntcodeProgram.IOScheme.CONSOLE)
+    input_queue = Queue()
+    output_queue = Queue()
+
+    program = IntcodeProgram(io_scheme=IntcodeProgram.IOScheme.QUEUE,
+                             input_queue=input_queue,
+                             output_queue=output_queue)
+
     program.initialize_memory_from_file('input.txt')
 
-    program.run_to_end()
+    input_queue.put(BLACK)
+
+    painting_robot = Robot()
+    painted_grid = defaultdict(int)
+
+    while True:
+        try:
+            program.execute_next()
+        except WaitingForInput:
+            new_color = output_queue.get()
+            direction = output_queue.get()
+
+            painted_grid[painting_robot.get_grid_pos()] = new_color
+            painting_robot.move(direction)
+
+            input_queue.put(painted_grid[painting_robot.get_grid_pos()])
+        except ProgramHalted:
+            print('Program Complete')
+            print(f'Painted {len(painted_grid.keys())} tiles at least once')
+            break
 
 
 if __name__ == '__main__':
