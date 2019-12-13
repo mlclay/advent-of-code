3aa936b1f87e1336155a1efa4f42c9c2fba1593d
Lucas Clay <38572212+mlclay@users.noreply.github.com>
2019-12-12 20:43:10 -0500

    2019-12 part 2 complete

--- 2019/12/aoc.py
+++ 2019/12/aoc.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 import re
+from functools import reduce
+from math import gcd
+
+
+def lcm(a, b):
+    return int(a * b / gcd(a, b))
+
+
+def lcms(*numbers):
+    return reduce(lcm, numbers)
 
 
 class Point:
@@ -89,7 +99,7 @@ def parse_input():
     return moons
 
 
-def main():
+def part_one():
     moons = parse_input()
 
     for step in range(1, 1001):
@@ -102,6 +112,133 @@ def main():
     for moon_num, moon in enumerate(moons):
         print(moon)
     print(f'System Total Energy: {sum([moon.total_energy for moon in moons])}')
+
+
+def part_two_slow():
+    moons = parse_input()
+    dimension_steps = {'x': 0, 'y': 0, 'z': 0}
+
+    initial_positions = {
+        'x': [moon.position.x for moon in moons],
+        'y': [moon.position.y for moon in moons],
+        'z': [moon.position.z for moon in moons],
+    }
+    initial_velocities = {
+        'x': [moon.velocity.x for moon in moons],
+        'y': [moon.velocity.y for moon in moons],
+        'z': [moon.velocity.z for moon in moons],
+    }
+
+    found_x_cycle = False
+    found_y_cycle = False
+    found_z_cycle = False
+
+    while True:
+        for moon_num, moon in enumerate(moons):
+            for next_moon in range(moon_num + 1, len(moons)):
+                moon.velocity += moon.position.step_delta(moons[next_moon].position)
+                moons[next_moon].velocity += moons[next_moon].position.step_delta(moon.position)
+            moon.apply_velocity()
+
+        if not found_x_cycle:
+            dimension_steps['x'] += 1
+            if initial_positions['x'] == [moon.position.x for moon in moons] and \
+                    initial_velocities['x'] == [moon.velocity.x for moon in moons]:
+                found_x_cycle = True
+
+        if not found_y_cycle:
+            dimension_steps['y'] += 1
+            if initial_positions['y'] == [moon.position.y for moon in moons] and \
+                    initial_velocities['y'] == [moon.velocity.y for moon in moons]:
+                found_y_cycle = True
+
+        if not found_z_cycle:
+            dimension_steps['z'] += 1
+            if initial_positions['z'] == [moon.position.z for moon in moons] and \
+                    initial_velocities['z'] == [moon.velocity.z for moon in moons]:
+                found_z_cycle = True
+
+        if found_x_cycle and found_y_cycle and found_z_cycle:
+            break
+
+    print(f'Steps in cycle: {dimension_steps}')
+    print(f'LCM: {lcms(*dimension_steps.values())}')
+
+
+def part_two_fast():
+    moons = parse_input()
+    dimension_steps = {'x': 0, 'y': 0, 'z': 0}
+
+    initial_positions = {
+        'x': [moon.position.x for moon in moons],
+        'y': [moon.position.y for moon in moons],
+        'z': [moon.position.z for moon in moons],
+    }
+    initial_velocities = {
+        'x': [moon.velocity.x for moon in moons],
+        'y': [moon.velocity.y for moon in moons],
+        'z': [moon.velocity.z for moon in moons],
+    }
+
+    found_x_cycle = False
+    found_y_cycle = False
+    found_z_cycle = False
+
+    while True:
+        for moon_num, moon in enumerate(moons):
+            for next_moon in range(moon_num + 1, len(moons)):
+                other = moons[next_moon]
+                if not found_x_cycle:
+                    delta = other.position.x - moon.position.x
+                    delta = (abs(delta) / delta) if delta else 0
+                    moon.velocity.x += delta
+                    other.velocity.x -= delta
+                if not found_y_cycle:
+                    delta = other.position.y - moon.position.y
+                    delta = (abs(delta) / delta) if delta else 0
+                    moon.velocity.y += delta
+                    other.velocity.y -= delta
+                if not found_z_cycle:
+                    delta = other.position.z - moon.position.z
+                    delta = (abs(delta) / delta) if delta else 0
+                    moon.velocity.z += delta
+                    other.velocity.z -= delta
+
+            if not found_x_cycle:
+                moon.position.x += moon.velocity.x
+            if not found_y_cycle:
+                moon.position.y += moon.velocity.y
+            if not found_z_cycle:
+                moon.position.z += moon.velocity.z
+
+        if not found_x_cycle:
+            dimension_steps['x'] += 1
+            if initial_positions['x'] == [moon.position.x for moon in moons] and \
+                    initial_velocities['x'] == [moon.velocity.x for moon in moons]:
+                found_x_cycle = True
+
+        if not found_y_cycle:
+            dimension_steps['y'] += 1
+            if initial_positions['y'] == [moon.position.y for moon in moons] and \
+                    initial_velocities['y'] == [moon.velocity.y for moon in moons]:
+                found_y_cycle = True
+
+        if not found_z_cycle:
+            dimension_steps['z'] += 1
+            if initial_positions['z'] == [moon.position.z for moon in moons] and \
+                    initial_velocities['z'] == [moon.velocity.z for moon in moons]:
+                found_z_cycle = True
+
+        if found_x_cycle and found_y_cycle and found_z_cycle:
+            break
+
+    print(f'Steps in cycle: {dimension_steps}')
+    print(f'LCM: {lcms(*dimension_steps.values())}')
+
+
+def main():
+    part_one()
+    part_two_fast()
 
 
 if __name__ == '__main__':
