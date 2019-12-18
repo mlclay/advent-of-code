97f6a7856f3dfb9bfd1567cd33df479083b6c14d
Lucas Clay <38572212+mlclay@users.noreply.github.com>
2019-12-17 19:24:34 -0500

    2019-17 part 2 complete

--- 2019/17/aoc.py
+++ 2019/17/aoc.py
@@ -69,6 +69,66 @@ def build_scaffolding(program):
     return scaffolding
 
 
+def draw_camera_image(image_data):
+    newline = image_data.index(10)
+    y = 0
+    while newline > 0:
+        line = image_data[:newline]
+        print(''.join([chr(x) for x in line]))
+        image_data = image_data[newline + 1:]
+        newline = image_data.index(10)
+        y += 1
+
+
+def drive_around(program, draw=False):
+    """
+    A     L,12,L,8,L,8,
+    B     L,12,R,4,L,12,R,6,
+    A     L,12,L,8,L,8,
+    C     R,4,L,12,L,12,R,6,
+    B     L,12,R,4,L,12,R,6,
+    A     L,12,L,8,L,8,
+    C     R,4,L,12,L,12,R,6,
+    A     L,12,L,8,L,8,
+    C     R,4,L,12,L,12,R,6,
+    B     L,12,R,4,L,12,R,6
+    """
+
+    main_routine = 'A,B,A,C,B,A,C,A,C,B'
+    routine_a = 'L,12,L,8,L,8'
+    routine_b = 'L,12,R,4,L,12,R,6'
+    routine_c = 'R,4,L,12,L,12,R,6'
+    visualize = 'n'
+
+    for input_data in [main_routine, routine_a, routine_b, routine_c, visualize]:
+        for character in input_data:
+            program.queue_input(ord(character))
+        program.queue_input(10)
+
+    image_data = []
+
+    while True:
+        try:
+            program.execute_next()
+            try:
+                output = program.get_output(block=False)
+                if output > 255:
+                    print(f'Part Two: {output}')
+                elif draw:
+                    image_data.append(output)
+                    if image_data[-1] == image_data[-2] == 10:
+                        draw_camera_image(image_data)
+                        image_data = []
+            except IndexError:
+                pass
+            except Empty:
+                pass
+
+        except ProgramHalted:
+            print('Program Complete')
+            break
+
+
 def main():
     input_queue = Queue()
     output_queue = Queue()
@@ -76,6 +136,7 @@ def main():
                              input_queue=input_queue,
                              output_queue=output_queue)
     program.initialize_memory_from_file('input.txt')
+    memory_dump = program.dump_memory()
     program.run_to_end()
 
     scaffolding = build_scaffolding(program)
@@ -88,6 +149,12 @@ def main():
 
     print(f'Part One: {sum(alignment_parameters)}')
 
+    program.load_memory(memory_dump)
+    program.set_memory_address(0, 2)
+
+    drive_around(program)
+
 
 if __name__ == '__main__':
     main()
+
