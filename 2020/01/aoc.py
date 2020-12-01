425a7e6524c14323625e80014705cf483d0e7314
mlclay <38572212+mlclay@users.noreply.github.com>
2020-12-01 12:22:33 -0500

    2020-01 part 2 complete

--- 2020/01/aoc.py
+++ 2020/01/aoc.py
@@ -1,22 +1,54 @@
 #!/usr/bin/python3
-from collections import defaultdict
 
 
-def two_sum():
-    values = defaultdict(bool)
+def parse_input():
+    """
+    Parse input.txt to list of masses
+    """
 
-    with open('input.txt', 'r') as input:
-        for value in input:
-            value = int(value.strip())
-            other = 2020 - value
-            if other in values:
-                return value, other
-            values[value] = True
+    values = []
+
+    with open('input.txt', 'r') as input_file:
+        for value in input_file:
+            values.append(int(value.strip()))
+
+    values.sort()
+    return values
+
+
+def two_sum(values, target=2020):
+    seen_values = set()
+
+    for value in values:
+        other = target - value
+        if other in seen_values:
+            return value, other
+        seen_values.add(value)
 
     return None
 
 
-if __name__ == '__main__':
-    sums = two_sum()
+def three_sum(values, target=2020):
+
+    for value in values:
+        other = target - value
+        sums = two_sum(values, target=other)
+        if sums:
+            return sums + (value,)
+
+    return None
+
+
+def main():
+    values = parse_input()
+    sums = two_sum(values)
     if sums:
         print(sums[0] * sums[1])
+
+    sums = three_sum(values)
+    if sums:
+        print(sums[0] * sums[1] * sums[2])
+
+
+if __name__ == '__main__':
+    main()
