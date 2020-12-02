2bd64ae3197e18fa5fc7cbab5527728b2847ced0
mlclay <38572212+mlclay@users.noreply.github.com>
2020-12-02 08:14:40 -0500

    2020-02 part 2 complete

--- 2020/02/aoc.py
+++ 2020/02/aoc.py
@@ -2,19 +2,28 @@
 from collections import Counter
 
 
+def parse_policy(policy):
+    count, check, password = policy.strip().split()
+    count = list(map(int, count.split('-')))
+    check = check[0]
+    return count, check, password
+
+
 def main():
-    valid = total = 0
+    part_one = part_two = 0
     with open('input.txt', 'r') as input_file:
         for policy in input_file:
-            total += 1
-            count, check, password = policy.strip().split()
-            count = list(map(int, count.split('-')))
-            check = check[0]
+            count, check, password = parse_policy(policy)
+
             counter = Counter(password)
             if count[0] <= counter[check] <= count[1]:
-                valid += 1
+                part_one += 1
 
-    print(valid)
+            if bool(password[count[0] - 1] == check) ^ bool(password[count[1] - 1] == check):
+                part_two += 1
+
+    print(f'Part One: {part_one}')
+    print(f'Part Two: {part_two}')
 
 
 if __name__ == '__main__':
